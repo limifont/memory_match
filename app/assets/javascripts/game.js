fba7e6eeebdf33154535f45fe761c2ff0339bdc6
Lindsey <lindsey.font@gmail.com>
2016-06-16 09:52:50 -0600

    Took out commented out code

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,22 +1,4 @@
 $(document).ready(function() {
-  // $('.mem-card').click(function() {
-  //   if($(this p).is(':hidden')) {
-  //     $(this).removeClass('hidden');
-  //   } else {
-  //     $(this).addClass('hidden');      
-  //   }
-  // });
-
-  // $('.mem-card').click(function() {
-  //   if($(this).has('p', function(){
-  //     if($(this).is(':hidden')) {
-  //       $(this).removeClass('hidden');
-  //     } else {
-  //       $(this).addClass('hidden');
-  //     }
-  //   });
-  // });
-
   $('.mem-card').click(function() {
     var hiddenPic = $(this).find('p');
     if(hiddenPic.is(':hidden')) {
@@ -26,14 +8,4 @@ $(document).ready(function() {
       hiddenPic.addClass('hidden');
     }
   });
-
-
-  // $('.grid-cell').click(function() {
-  //   if($(this).hasClass('blue-cell')) {
-  //     $(this).text('.').removeClass('blue-cell');
-  //   } else {
-  //     $(this).text('clicked').addClass('blue-cell');      
-  //   }
-  // });
-
 });
